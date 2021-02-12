6861d09a2eca9f4ce96a87bafa9851a8b2687a9a
Alejandro Perdomo <alejousco0708@gmail.com>
2021-02-11 21:58:58 -0500

    Panel completo

--- factoria/src/App.js
+++ factoria/src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import Nofound from './components/layout/Nofound';
+import Panel from './components/panel/Panel';
 
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
       <Switch>
         <Route exact path="/" component={Login}/>
         <Route exact path="/register" component={Register}/>
+        <Route exact path="/nofound" component={Nofound}/>
+        <Route exact path="/panel" component={Panel}/>
       </Switch>
     </Router>
   );
